1844b55d7ddb59f8c0f8c0491fd44e81e6fdd629
arani <aranimontes@protonmail.com>
2023-02-18 16:56:47 +0100

    basic backend done

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 //https://www.youtube.com/watch?v=qwM23_kF4v4   min29:20
 
-//api key: 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
+const { Configuration, OpenAIApi } = require("openai");
+const express = require("express");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const { Configuration, OpenAIApi } = require("openai");
-console.log("process.env.REACT_APP_OPENAI_KEY");
-console.log(process.env.TEST);
-//console.log(process.env);
+//connect to OpenAI
 const configuration = new Configuration({
   organization: "org-MilsZjmhcGi0CzP9I70FfIXK",
   apiKey: process.env.OPENAI_KEY,
@@ -15,7 +13,12 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 //const response = await openai.listEngines();
 
-async function callApi() {
+//create API
+
+const app = express();
+const port = 3080;
+
+app.post("/", async (req, res) => {
   const response = await openai.createCompletion({
     model: "text-davinci-003",
     prompt: "Say this is a test",
@@ -23,6 +26,9 @@ async function callApi() {
     temperature: 0,
   });
   console.log(response.data.choices[0].text);
-}
+  res.json({ data: response.data });
+});
 
-callApi();
+app.listen(port, () => {
+  console.log(`App listening at localhost ${port}`);
+});
